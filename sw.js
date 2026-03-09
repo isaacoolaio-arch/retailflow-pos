698032cd02f80303d920e495fcdbf4e4df98a085
isaacoolaio-arch <isaac.oola.io@gmail.com>
2026-03-09 21:45:24 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+var CACHE_NAME = 'retailflow-v3';
+var ASSETS = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(ASSETS);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(
+                keys.filter(function(k) { return k !== CACHE_NAME; })
+                    .map(function(k) { return caches.delete(k); })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    var url = event.request.url;
+    if (url.indexOf('script.google.com') !== -1 || url.indexOf('api.anthropic.com') !== -1) {
+        event.respondWith(
+            fetch(event.request).catch(function() {
+                return new Response(JSON.stringify({error:'offline'}), {headers:{'Content-Type':'application/json'}});
+            })
+        );
+        return;
+    }
+    if (event.request.method !== 'GET') return;
+    event.respondWith(
+        caches.match(event.request).then(function(cached) {
+            return cached || fetch(event.request);
+        })
+    );
+});
