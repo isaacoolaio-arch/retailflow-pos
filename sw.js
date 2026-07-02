71f4d700f6c23d4dcefe2d445360b0b77d3f65aa
isaacoolaio-arch <isaac@oolapos.com>
2026-07-02 17:21:26 +0000

    v18: Force cache bust — clear old service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'oola-retailflow-v5';
+var CACHE_NAME = 'oola-retailflow-v18';
 var ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', function(event) {
