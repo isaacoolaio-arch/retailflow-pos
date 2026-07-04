0bcb61034946c589b99bcde3e33addddc469a3c9
isaacoolaio-arch <isaac@oolapos.com>
2026-07-04 19:00:38 +0000

    v22: Replace html5-qrcode with native getUserMedia + BarcodeDetector — fixes Samsung black screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'oola-retailflow-v21';
+var CACHE_NAME = 'oola-retailflow-v22';
 var ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', function(event) {
